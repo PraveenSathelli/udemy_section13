42b1de05a916b8be9c245c643c40a5758815619f
Praveen Sathelli <praveensathelli11@gmail.com>
2026-09-14 17:16:36 +0530

    TEMPLATE DRIVEN FORM WITH VALIDATION

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports:[FormsModule]
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  onSuccess(loginForm:NgForm) {
+    console.log("login form");
+    console.log(loginForm)
+  }
+
+}
