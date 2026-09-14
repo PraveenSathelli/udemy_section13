0ba7a29d53a5e76c8bf74a2cabbe472cf8f29b48
Praveen Sathelli <praveensathelli11@gmail.com>
2026-09-14 22:52:47 +0530

    viewChild, DestryRef, debounceTime

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,18 +1,50 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, OnInit, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports:[FormsModule]
+  imports: [FormsModule]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  onSuccess(loginForm:NgForm) {
+  private form = viewChild.required<NgForm>('loginForm');
+  private objDestroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form().valueChanges?.pipe(
+        debounceTime(500)
+      ).subscribe({
+        next: (response) => {
+          console.log(response);
+          window.localStorage.setItem('saved-username', JSON.stringify(response));
+        }
+      });
+
+      this.objDestroyRef.onDestroy(() => {
+        subscription?.unsubscribe();
+      });
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onSuccess(loginForm: NgForm) {
+
+    if (loginForm.invalid)
+      return true;
+
     console.log("login form");
     console.log(loginForm)
+
+    loginForm.reset();
+    return false;
   }
 
 }
